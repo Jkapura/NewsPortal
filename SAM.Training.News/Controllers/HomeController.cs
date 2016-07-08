33fb2b38e5ec4a1235b3793bf46c7d1e20efc98a
Jkapura <kapura.yulya@gmail.com>
2016-07-08 17:41:39 +0300

    Basic styles, _layout, context included.

---
+++ SAM.Training.News/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SAM.Training.News.Models;
+
+namespace SAM.Training.News.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        mapclassDataContext db = new mapclassDataContext();
+
+        IEnumerable<Article> _articles { get; set; }
+        IEnumerable<Article> Articles
+        {
+            get
+            {
+                if (_articles == null)
+                {
+                    _articles = db.Articles;
+                }
+
+                return _articles;
+            }
+        }
+
+        //ArticleContext db = new ArticleContext();
+        public ActionResult Index()
+        {
+            
+               db.Articles.InsertOnSubmit(new Article()
+              {
+                   authorId = 1,
+                  categoryId = 1,
+                  head = "asd"
+               });
+             
+
+              db.SubmitChanges();
+                          
+                ViewBag.Articles = Articles;
+            
+            return View();
+        }
+
+    }
+}
---
+++ SAM.Training.News/Scripts/indexScript.js
@@ -0,0 +1,37 @@
+﻿$(function () {
+    $(window).scroll(function () {
+        var top = $(document).scrollTop();
+        if (top < 100)
+            $(".navpanel").css({ top: '0', position: 'relative' });
+        else $(".navpanel").css({ top: '0', position: 'fixed' });
+
+    });
+});
+
+function windowSize() {
+    if ($(window).width() <= '1600') {
+
+        $('.aLight').hide();
+    }
+    else {
+        $(".aLight").show();
+    }
+}
+$(window).on('load resize', windowSize);
+$(function () {
+    $("#logo").click(function () {
+        $(".hiddenBar").slideToggle("slow");
+        var top = $(document).scrollTop();
+        if (top < 100)
+            $(".hiddenBar").css({ top: '100', position: 'fixed' });
+        else $(".hiddenBar").css({ top: '-30', position: 'fixed' });
+    });
+});
+
+//function hiddenBarScroll(){
+//    var top = $(document).scrollTop();
+//    if (top < 100)
+//        $(".navpanel").css({ top: '0', position: 'relative' });
+//    else 
+//        $(".navpanel").css({ top: '60', position: 'fixed' });
+//}
