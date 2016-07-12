76c2f15b8aaf92e2701388e046ddec74e70668b6
Jkapura <kapura.yulya@gmail.com>
2016-07-12 18:05:59 +0300

    AddControl, View, new styles

--- SAM.Training.News/Controllers/HomeController.cs
+++ SAM.Training.News/Controllers/HomeController.cs
@@ -15,6 +15,8 @@ namespace SAM.Training.News.Controllers
         mapclassDataContext db = new mapclassDataContext();
 
         IEnumerable<Article> _articles { get; set; }
+        IEnumerable<Author> _authors { get; set; }
+
         IEnumerable<Article> Articles
         {
             get
@@ -27,25 +29,44 @@ namespace SAM.Training.News.Controllers
                 return _articles;
             }
         }
+        IEnumerable<Author> Authors
+        {
+            get
+            {
+                if (_authors == null)
+                {
+                    _authors = db.Authors;
+                }
 
-        //ArticleContext db = new ArticleContext();
+                return _authors;
+            }
+        }
+        
         public ActionResult Index()
         {
-            
-               db.Articles.InsertOnSubmit(new Article()
-              {
-                   authorId = 1,
-                  categoryId = 1,
-                  head = "asd"
-               });
+               
+              // db.Articles.InsertOnSubmit(new Article()
+              //{
+              //     authorId = 1,
+              //    categoryId = 1,
+              //    head = "asd"
+              // });
              
 
               db.SubmitChanges();
                           
                 ViewBag.Articles = Articles;
             
-            return View();
+            return View("Index");
         }
-
+        public ActionResult ArticleView(int id)
+        {
+            ViewBag.article = Articles.First(i => i.id.Equals(id));
+            ViewBag.author = Authors.First(i => ViewBag.article.authorId.Equals(i.id));
+            
+                return View("ArticleView");
+            
+            
+        }
     }
 }
